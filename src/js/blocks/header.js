2a3e192dc5f7ecbf08e96b572a65c29cda0ac370
SpDmitryKalinin <spkalinin97@gmail.com>
2022-06-19 16:12:03 +0300

    finish header

--- src/js/blocks/header.js
+++ src/js/blocks/header.js
@@ -1,5 +1,19 @@
 const header = document.querySelector('.header');
 
 if(header) {
-    
+    const burger = header.querySelector('.header__burger');
+    const langSwitcher = header.querySelector('.lang-switcher');
+    burger.addEventListener('click', () => {
+        burger.style.opacity = 0;
+        langSwitcher.style.opacity = 0;
+        burger.style.transition = '';
+        langSwitcher.style.transition = '';
+        header.classList.toggle('header--active');
+        setTimeout(()=> {
+            burger.style.transition = '0.5s ease-in-out';
+            langSwitcher.style.transition = '0.5s ease-in-out';
+            burger.style.opacity = 1;
+            langSwitcher.style.opacity = 1;
+        }, 500)
+    })
 }
